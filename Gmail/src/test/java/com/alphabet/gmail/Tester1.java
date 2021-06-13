37d2405dcd61633896279c2ce4c7cca7147c2920
hemanth4sure <nameishemanth490@gmail.com>
2021-06-13 11:16:38 +0530

    "1st Commit"

---
+++ Gmail/src/test/java/com/alphabet/gmail/Tester1.java
@@ -0,0 +1,18 @@
+package com.alphabet.gmail;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+
+public class Tester1 {
+
+	public static void main(String[] args) {
+		WebDriverManager.chromedriver().setup();
+		WebDriver driver = new ChromeDriver();
+		driver.get("https://www.google.com");
+		driver.findElement(By.name("q")).sendKeys("Hello");
+	}
+
+}
